6c9ec65a4f46102be64c8f980d8c6f3b63af605f
Davonta Hubbard <davonta011@yahoo.com>
2020-01-28 19:18:03 -0600

    discovery service added

---
+++ PortfolioDiscoveryService/src/main/java/com/porfolio/PortfolioDiscoveryService/PortfolioDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.PortfolioDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ PortfolioDiscoveryService/src/test/java/com/porfolio/PortfolioDiscoveryService/PortfolioDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.PortfolioDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
