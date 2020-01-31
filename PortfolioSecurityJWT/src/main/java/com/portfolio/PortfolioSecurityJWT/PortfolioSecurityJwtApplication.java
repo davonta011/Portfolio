0600667cd5d378db3d9c181e765879c280b93175
Davonta Hubbard <davonta011@yahoo.com>
2020-01-30 22:41:06 -0600

    jwt security

---
+++ PortfolioSecurityJWT/src/main/java/com/portfolio/PortfolioSecurityJWT/PortfolioSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ PortfolioSecurityJWT/src/test/java/com/portfolio/PortfolioSecurityJWT/PortfolioSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
