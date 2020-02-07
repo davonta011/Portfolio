15d44c73c8a6ad0cf1d784da583dbf5d14095d9f
Ben, Richard <benjamin.barrett@cognizant.com, richard.steward@cognizant.com>
2020-02-06 20:53:43 -0600

    zuul api gateway

---
+++ PortfolioApiGateway/src/main/java/com/Portfolio/ApiGateway/PortfolioApiGateway/PortfolioApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.Portfolio.ApiGateway.PortfolioApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class PortfolioApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApiGatewayApplication.class, args);
+	}
+
+}
---
+++ PortfolioApiGateway/src/test/java/com/Portfolio/ApiGateway/PortfolioApiGateway/PortfolioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.ApiGateway.PortfolioApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
